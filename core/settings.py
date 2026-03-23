2075447e26e9001cddfd0f756a4b167f2722c226
Ismail Muhammad <ismaeelmuhammad123@gmail.com>
2026-03-23 17:48:08 +0100

    Optimize database connections for production and ensure all latest store upgrades are pushed.

--- core/settings.py
+++ core/settings.py
@@ -98,7 +98,7 @@ if not DEBUG:
     DATABASES = {
         'default': dj_database_url.config(
             default=os.getenv('DATABASE_URL'),
-            conn_max_age=600
+            conn_max_age=0
         )
     }
 
